a0079392bfdeebcd94018f099fe8f61a2a4e4757
dovanan95 <dovanan95@gmail.com>
2022-06-18 04:44:20 +0900

    init_secure_chat

--- fabcar/javascript/views_h/js/helper.js
+++ fabcar/javascript/views_h/js/helper.js
@@ -70,6 +70,10 @@ function decryptMyMessage(message){
         else if(decodedMessage.signature=='unsigned'){
             return 'tin nhan khong an toan';
         }
+        else if(decodedMessage.status == 'failure')
+        {
+            return '***********************';
+        }
 
     }catch(error){
         console.log(error);
@@ -113,3 +117,83 @@ function decryptMyMessage(message){
     }
 
     //----------------End Call--------------------------//
+
+    //------------------Search User-----------------------
+    function el_partner_connect(id, name, position, dept, Phone, rank)
+    {
+        var element_partner_selection_element =
+        `
+            <div class="tablinks" username="${name}"" id="${tab_partner_prefix+id}" dept="${dept}" rank="${rank}"
+                onclick={connect_user(this)} id_user="${id}" position="${position}" Phone="${Phone}">
+                <div class="acc-status">
+                    <div class="acc">${name}</div>
+                    <div class="status-online">${rank}</div>
+                </div>
+            </div>
+            <hr class="solid">
+        `
+        return element_partner_selection_element;
+    }
+    var id = document.getElementById("input_ID");
+
+    id.addEventListener("keyup", function(e){
+        if (e.keyCode === 13){
+        search();
+        }
+    })
+    var user_obj;
+    async function search()
+    {
+        if(myID==id.value) {
+        alert('this is your ID');
+        } 
+        else
+        {
+            const options = {
+                method: 'GET',
+                headers: {
+                    'authorization': 'token '+ accessToken,
+                }
+            }
+            var res = await fetch('/searchUserByID?id='+id.value, options);
+            var res_json = await res.json();
+            console.log(res_json);
+            if(res_json.data == 'no_data')
+            {
+                infor.innerHTML='no information';
+            }
+            else if(res_json.data != 'no_data')
+                {
+                    left_tab.innerHTML='';
+                    for(let i=0;i<res_json.data.length;i++)
+                    {
+                        var el_connect= el_partner_connect(res_json.data[i].id,res_json.data[i].TenDayDu,
+                        res_json.data[i].chuc_vu, res_json.data[i].TenDonVi, res_json.data[i].Mobile, res_json.data[i].cap_bac);
+                        left_tab.insertAdjacentHTML("beforeend", el_connect);
+                    }
+                    sessionStorage.setItem('temp_list_connect', JSON.stringify(res_json.data));
+                }
+        }
+        
+    }
+
+    function connect_user(obj)
+    {
+        var searchUrl = "/user_information";
+        var connect_list = JSON.parse(sessionStorage.getItem('temp_list_connect'));
+        var uid = obj.getAttribute('id_user');
+        var user_obj;
+        for(let i=0;i<connect_list.length;i++)
+        {
+            if(uid==connect_list[i].id)
+            {
+                searchUrl=searchUrl+'?id_user='+connect_list[i].id;
+                console.log(searchUrl);
+                window.location.href = searchUrl;
+                //window.open(searchUrl);
+            }
+        }
+        
+    }
+
+    //---------------End Search User-------------------------------
