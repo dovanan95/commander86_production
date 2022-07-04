2c90076b213c4e74d7e348aec2add1891f8a57ab
dovanan95 <dovanan95@gmail.com>
2022-07-05 04:42:58 +0900

    update_blockchain_script

--- smart_contract_upgrade.sh
+++ smart_contract_upgrade.sh
@@ -2,7 +2,7 @@ export PATH=${PWD}/../bin:$PATH
 export FABRIC_CFG_PATH=$PWD/../config/
 export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
 
-peer lifecycle chaincode package fabcar_5.tar.gz --path ../chaincode/fabcar/javascript/ --lang node --label fabcar_5.0
+peer lifecycle chaincode package chatblockchain_2.tar.gz --path ../chaincode/fabcar/javascript/ --lang node --label chatblockchain_2.0
 
 export CORE_PEER_TLS_ENABLED=true
 
@@ -11,34 +11,93 @@ export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/organizations/peerOrganizations/org1.e
 export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
 export CORE_PEER_ADDRESS=localhost:7051
 
-peer lifecycle chaincode install fabcar_5.tar.gz #return chaincode_package_id
+peer lifecycle chaincode install chatblockchain_2.tar.gz #return chaincode_package_id
 
-export NEW_CC_PACKAGE_ID=fabcar_5.0:b8e25f72c8c5516f707bd5db0c36fd19d38f0bd194094da3e4cc14f62e82e019 
+export NEW_CC_PACKAGE_ID=chatblockchain_2.0:b8e25f72c8c5516f707bd5db0c36fd19d38f0bd194094da3e4cc14f62e82e019 
 # 'b8e25f72c8c5516f707bd5db0c36fd19d38f0bd194094da3e4cc14f62e82e019' is chaincode_package_id returned from previous step 
 
-peer lifecycle chaincode approveformyorg -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com --channelID mychannel --name fabcar --version 5.0 --package-id $NEW_CC_PACKAGE_ID --sequence 4 --tls --cafile "${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem"
+peer lifecycle chaincode approveformyorg -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com --channelID mangblockchainquansu --name chatblockchain --version 2.0 --package-id $NEW_CC_PACKAGE_ID --sequence 1 --tls --cafile "${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem"
 
 export CORE_PEER_LOCALMSPID="Org2MSP"
 export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/organizations/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt
 export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp
 export CORE_PEER_ADDRESS=localhost:9051
 
-peer lifecycle chaincode install fabcar_5.tar.gz
+peer lifecycle chaincode install chatblockchain_2.tar.gz
 
-peer lifecycle chaincode approveformyorg -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com --channelID mychannel --name fabcar --version 5.0 --package-id $NEW_CC_PACKAGE_ID --sequence 4 --tls --cafile "${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem"
+peer lifecycle chaincode approveformyorg -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com --channelID mangblockchainquansu --name chatblockchain --version 2.0 --package-id $NEW_CC_PACKAGE_ID --sequence 1 --tls --cafile "${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem"
 
 export CORE_PEER_LOCALMSPID="Org3MSP"
 export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/organizations/peerOrganizations/org3.example.com/peers/peer0.org3.example.com/tls/ca.crt
 export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org3.example.com/users/Admin@org3.example.com/msp
 export CORE_PEER_ADDRESS=localhost:11051
 
-peer lifecycle chaincode install fabcar_5.tar.gz
+peer lifecycle chaincode install chatblockchain_2.tar.gz
 
-peer lifecycle chaincode approveformyorg -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com --channelID mychannel --name fabcar --version 5.0 --package-id $NEW_CC_PACKAGE_ID --sequence 4 --tls --cafile "${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem"
+peer lifecycle chaincode approveformyorg -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com --channelID mangblockchainquansu --name chatblockchain --version 2.0 --package-id $NEW_CC_PACKAGE_ID --sequence 1 --tls --cafile "${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem"
 
 
-peer lifecycle chaincode checkcommitreadiness --channelID mychannel --name fabcar --version 5.0 --sequence 5 --tls --cafile "${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem" --output json
+peer lifecycle chaincode checkcommitreadiness --channelID mangblockchainquansu --name chatblockchain --version 2.0 --sequence 1 --tls --cafile "${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem" --output json
 
-peer lifecycle chaincode commit -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com --channelID mychannel --name fabcar --version 5.0 --sequence 4 --tls --cafile "${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem" --peerAddresses localhost:7051 --tlsRootCertFiles "${PWD}/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt" --peerAddresses localhost:9051 --tlsRootCertFiles "${PWD}/organizations/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt" --peerAddresses localhost:11051 --tlsRootCertFiles "${PWD}/organizations/peerOrganizations/org3.example.com/peers/peer0.org3.example.com/tls/ca.crt"
+peer lifecycle chaincode commit -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com --channelID mangblockchainquansu --name chatblockchain --version 2.0 --sequence 1 --tls --cafile "${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem" --peerAddresses localhost:7051 --tlsRootCertFiles "${PWD}/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt" --peerAddresses localhost:9051 --tlsRootCertFiles "${PWD}/organizations/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt" --peerAddresses localhost:11051 --tlsRootCertFiles "${PWD}/organizations/peerOrganizations/org3.example.com/peers/peer0.org3.example.com/tls/ca.crt"
 
-peer chaincode query -C mychannel -n fabcar -c '{"Args":["GetAllAssets"]}'
+peer chaincode query -C mangblockchainquansu -n chatblockchain -c '{"Args":["queryAllData"]}'
+
+
+
+
+
+#PRODUCTION IMPLEMENTATION
+
+export PATH=${PWD}/../bin:$PATH
+export FABRIC_CFG_PATH=$PWD/../config/
+export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
+export PACKAGE_NAME=chatblockchain_4.tar.gz
+export CHAINCODE_LABEL=chatblockchain_4.0
+export CHAINCODE_NAME=chatblockchain
+export CHANNEL_ID=mangblockchainquansu
+export version=4.0
+export sequence=4
+
+peer lifecycle chaincode package $PACKAGE_NAME --path ../chaincode/fabcar/javascript/ --lang node --label $CHAINCODE_LABEL
+
+export CORE_PEER_TLS_ENABLED=true
+
+export CORE_PEER_LOCALMSPID="Org1MSP"
+export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt
+export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
+export CORE_PEER_ADDRESS=localhost:7051
+
+peer lifecycle chaincode install $PACKAGE_NAME #return chaincode_package_id
+
+export NEW_CC_PACKAGE_ID=chatblockchain_2.0:b8e25f72c8c5516f707bd5db0c36fd19d38f0bd194094da3e4cc14f62e82e019 
+# 'b8e25f72c8c5516f707bd5db0c36fd19d38f0bd194094da3e4cc14f62e82e019' is chaincode_package_id returned from previous step (DO NOT COPY)
+
+peer lifecycle chaincode approveformyorg -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com --channelID $CHANNEL_ID --name $CHAINCODE_NAME --version $version --package-id $NEW_CC_PACKAGE_ID --sequence $sequence --tls --cafile "${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem"
+
+export CORE_PEER_LOCALMSPID="Org2MSP"
+export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/organizations/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt
+export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp
+export CORE_PEER_ADDRESS=localhost:9051
+
+peer lifecycle chaincode install $PACKAGE_NAME
+
+peer lifecycle chaincode approveformyorg -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com --channelID $CHANNEL_ID --name $CHAINCODE_NAME --version $version --package-id $NEW_CC_PACKAGE_ID --sequence $sequence --tls --cafile "${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem"
+
+export CORE_PEER_LOCALMSPID="Org3MSP"
+export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/organizations/peerOrganizations/org3.example.com/peers/peer0.org3.example.com/tls/ca.crt
+export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org3.example.com/users/Admin@org3.example.com/msp
+export CORE_PEER_ADDRESS=localhost:11051
+
+peer lifecycle chaincode install $PACKAGE_NAME
+
+peer lifecycle chaincode approveformyorg -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com --channelID $CHANNEL_ID --name $CHAINCODE_NAME --version $version --package-id $NEW_CC_PACKAGE_ID --sequence $sequence --tls --cafile "${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem"
+
+
+peer lifecycle chaincode checkcommitreadiness --channelID $CHANNEL_ID --name $CHAINCODE_NAME --version $version --sequence $sequence --tls --cafile "${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem" --output json
+
+peer lifecycle chaincode commit -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com --channelID $CHANNEL_ID --name $CHAINCODE_NAME --version $version --sequence $sequence --tls --cafile "${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem" --peerAddresses localhost:7051 --tlsRootCertFiles "${PWD}/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt" --peerAddresses localhost:9051 --tlsRootCertFiles "${PWD}/organizations/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt" --peerAddresses localhost:11051 --tlsRootCertFiles "${PWD}/organizations/peerOrganizations/org3.example.com/peers/peer0.org3.example.com/tls/ca.crt"
+
+peer chaincode query -C $CHANNEL_ID -n $CHAINCODE_NAME -c '{"Args":["queryAllData"]}'
+
+docker update --restart unless-stopped $(docker ps -q)
