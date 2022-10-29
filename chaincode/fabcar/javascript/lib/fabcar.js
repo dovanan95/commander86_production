33605bdd221f469c451bc72656988387eb0842b6
ibm_dovanan <dovanan95@gmail.com>
2022-10-30 00:44:53 +0700

    logic

--- chaincode/fabcar/javascript/lib/fabcar.js
+++ chaincode/fabcar/javascript/lib/fabcar.js
@@ -136,13 +136,14 @@ class FabCar extends Contract {
             };
             let timestamp = new Date.now();
             var result = await this.queryCustom(ctx, JSON.stringify(query_officer_check)); console.log(result);
-            if(result || JSON.parse(result.toString()).length>0){
-                return ('user already registered!');
+            
+            if(!result || JSON.parse(result.toString()).length==0){
+                let quanNhan = await ctx.stub.putState(SoHieuQuanNhan.toString(), Buffer.from(JSON.stringify(quanNhan)));
+                let lichSu = await ctx.stub.putState('lichSu'+ SoHieuQuanNhan.toString()+ timestamp.toString(), Buffer.from(JSON.stringify(lichSuCapNhatQuanNhan)))
+                return(quanNhan);
             }
-            else if(!result || JSON.parse(result.toString()).length==0){
-                await ctx.stub.putState(SoHieuQuanNhan.toString(), Buffer.from(JSON.stringify(quanNhan)));
-                await ctx.stub.putState('lichSu'+ SoHieuQuanNhan.toString()+ timestamp.toString(), Buffer.from(JSON.stringify(lichSuCapNhatQuanNhan)))
-                return('finish');
+            else if(result || JSON.parse(result.toString()).length>0){
+                return ('user already registered!');
             }
         }
         catch(error){
