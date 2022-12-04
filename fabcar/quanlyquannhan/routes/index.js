ec0bacdee43324f62529dc1fcd43244de0bdb78d
ibm_dovanan <dovanan95@gmail.com>
2022-12-04 21:46:04 +0700

    j

--- fabcar/quanlyquannhan/routes/index.js
+++ fabcar/quanlyquannhan/routes/index.js
@@ -198,7 +198,11 @@ router.get('/timkiem', async function (req, res, next) {
     }
     queryParam.docType = "QuanNhan";
     let queryString = {
-      "selector": queryParam, //'limit': parseInt(limit), 'skip': parseInt(skip), "use_index": ["_design/indexPrivMessDoc", "indexPrivMess"]
+      "selector": queryParam,
+      "sort": [{ "HoVaTen": "desc" }],
+      'limit': parseInt(limit),
+      'skip': parseInt(skip),
+      "use_index": ["_design/indexPrivMessDoc", "indexPrivMess"]
     }
     console.log('queryString', queryString)
     const thongTinQuanNhan = await contract_.evaluateTransaction('queryCustom', JSON.stringify(queryString));
