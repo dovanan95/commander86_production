89756d5014b1bcc41e9b4dd6fba9f6137b5b02b3
ibm_dovanan <dovanan95@gmail.com>
2022-12-18 16:19:51 +0700

    d

--- fabcar/quanlyquannhan/routes/index.js
+++ fabcar/quanlyquannhan/routes/index.js
@@ -772,14 +772,14 @@ router.post('/blockchain-verify', async function (req, res, next) {
       }
     }
     // const thongTinQuanNhan = await contract_.evaluateTransaction('queryUser', body.user_id);
-    const thongTinQuanNhan = await contract_.evaluateTransaction('queryCustom', queryString);
-    let beforeVerify = await JSON.parse(thongTinQuanNhan.toString())[0];
+    const thongTinQuanNhan = await contract_.evaluateTransaction('queryCustom', JSON.stringify(queryString));
+    let beforeVerify = await JSON.parse(thongTinQuanNhan.toString())[0].Record;
     console.log('bf', beforeVerify);
     let key = body.user_id;
     let afterResult = await contract_.submitTransaction('verifyMessBlockchain', key, new Date().getTime());
 
     let afterVerify = await JSON.parse(afterResult.toString());
-    let ObjA = beforeVerify.Record;
+    let ObjA = beforeVerify;
     let ObjB = afterVerify;
     let diff = await validateInput.verifyBlockchainData(ObjA, ObjB);
     res.status(200).send({ 'statusCode': res.statusCode, 'message': diff });
