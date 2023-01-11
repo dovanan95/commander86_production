36f14aa9bea8416f4863aa128c24348885a81699
ibm_dovanan <dovanan95@gmail.com>
2023-01-12 01:50:54 +0700

    km

--- fabcar/quanlyquannhan/routes/index.js
+++ fabcar/quanlyquannhan/routes/index.js
@@ -273,6 +273,9 @@ router.get('/timkiemthongke', async function (req, res, next) {
     let query = req.query;
     let filter = query.filter;
     let value = decodeURIComponent(query.value); console.log('value', query.value, value);
+    if (value == "") {
+      value = '';
+    }
     let limit = query.limit;
 
     let skip;
